d35951fff6b399f183dea1693a955bc58c535a4d
Vitaliy Demchyshyn <demchyshyn.v87@gmail.com>
2023-10-12 21:14:23 +0300

    hw4

--- apps/cars/filters.py
+++ apps/cars/filters.py
@@ -5,16 +5,28 @@ from apps.cars.models import CarModel
 
 
 class CarFilter(filters.FilterSet):
-    yearLt = filters.NumberFilter('year', 'lt')
-    year_gt = filters.NumberFilter('year', 'gt')
-    year = filters.RangeFilter('year')
-    brand_contains = filters.CharFilter('brand', 'icontains')
-    body = filters.ChoiceFilter('body', choices=BodyTypeChoices.choices)
+    # yearLt = filters.NumberFilter('year', 'lt')
+    # year_gt = filters.NumberFilter('year', 'gt')
+    # year = filters.RangeFilter('year')
+    # brand_contains = filters.CharFilter('brand', 'icontains')
+    # body = filters.ChoiceFilter('body', choices=BodyTypeChoices.choices)
     order = filters.OrderingFilter(
         fields=(
             'id',
             'brand',
             'year',
-            ('price', 'asd')
+            'price',
+            'body',
+            'crated_at',
+            'updated_at'
         )
     )
+
+    class Meta:
+        model = CarModel
+        fields = {
+            'year': ('lt', 'lte', 'gt', 'gte'),
+            'price': ('lt', 'lte', 'gt', 'gte'),
+            'body': ('istartswith', 'iendswith', 'icontains'),
+            'brand': ('istartswith', 'iendswith', 'icontains')
+        }
